da71115334eed9a0feeac02b569bce98d2a7f7d2
Satyam <satyampandit021@gmail.com>
2023-06-17 22:18:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+
+let audio1 = () => {
+
+    let x = document.getElementById("aud1");
+
+    x.play();
+
+
+}
+let audio2 = () => {
+
+    let x = document.getElementById("aud2");
+
+    x.play();
+
+
+}
+
+let audio3 = () => {
+
+    let x = document.getElementById("aud3");
+
+    x.play();
+
+
+}
+let audio4 = () => {
+
+    let x = document.getElementById("aud4");
+
+    x.play();
+
+
+}
+let audio5 = () => {
+
+    let x = document.getElementById("aud5");
+
+    x.play();
+
+
+}
+
+let audio6 = () => {
+
+    let x = document.getElementById("aud6");
+
+    x.play();
+
+
+}
+let audio7 = () => {
+
+    let x = document.getElementById("aud7");
+
+    x.play();
+
+
+}
+
+
+
+
+
